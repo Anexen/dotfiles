4d212d38686ccd7a1551f32e2f54098fedeea399
Anexen <avolk93@gmail.com>
2020-02-17 17:21:47 +0300

    update bash fzf config

--- bash/.config/bash/plugins/fzf.bash
+++ bash/.config/bash/plugins/fzf.bash
@@ -14,47 +14,40 @@ export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat 
 # fe [FUZZY PATTERN] - Open the selected file with the default editor
 #   - Bypass fuzzy finder if there's only one match (--select-1)
 #   - Exit if there's no match (--exit-0)
-fe() {
-  local files
-  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
-  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+function fe() {
+    local files
+    IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+
+    [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
 }
 
-# fkill - kill processes - list only the ones you can kill
-fkill() {
-    local pid
-    if [ "$UID" != "0" ]; then
-        pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
-    else
-        pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
-    fi
-
-    if [ "x$pid" != "x" ]
-    then
-        echo $pid | xargs kill -${1:-9}
-    fi
+# git checkout branch
+function gcb() {
+    local branch
+    branch=$(git for-each-ref --format='%(refname:short)' refs/heads | fzf -q "$1")
+    [[ -n "$branch" ]] && git checkout $branch
 }
 
 # Select a docker container to start and attach to
-function da() {
-  local cid
-  cid=$(docker ps -a | sed 1d | fzf -1 -q "$1" | awk '{print $1}')
+function dca() {
+    local cid
+    cid=$(docker ps -a | sed 1d | fzf -1 -q "$1" | awk '{print $1}')
 
-  [ -n "$cid" ] && docker start "$cid" && docker attach "$cid"
+    [[ -n "$cid" ]] && docker start "$cid" && docker attach "$cid"
 }
 
 # Select a running docker container to stop
-function ds() {
-  local cid
-  cid=$(docker ps | sed 1d | fzf -q "$1" | awk '{print $1}')
+function dcs() {
+    local cid
+    cid=$(docker ps | sed 1d | fzf -q "$1" | awk '{print $1}')
 
-  [ -n "$cid" ] && docker stop "$cid"
+    [[ -n "$cid" ]] && docker stop "$cid"
 }
 
 # Select a docker container to remove
-function drm() {
-  local cid
-  cid=$(docker ps -a | sed 1d | fzf -q "$1" | awk '{print $1}')
+function dcr() {
+    local cid
+    cid=$(docker ps -a | sed 1d | fzf -q "$1" | awk '{print $1}')
 
-  [ -n "$cid" ] && docker rm "$cid"
+    [[ -n "$cid" ]] && docker rm "$cid"
 }
