12ece6e60242444330c7a0e7a085a36d75216623
Anexen <avolk93@gmail.com>
2020-07-02 17:43:13 +0300

    pyenv get rid of shims

--- home/.config/bash/plugins/pyenv.bash
+++ home/.config/bash/plugins/pyenv.bash
@@ -5,9 +5,17 @@ export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 
 path_add "${PYENV_ROOT}/bin"
 
-if _command_exists pyenv; then
-  eval "$(pyenv init - bash)"
-fi
+source "${PYENV_ROOT}/completions/pyenv.bash"
+
+workon () {
+    local _name="${1:-$(cat .python-version)}"
+    source "${PYENV_ROOT}/versions/${_name}/bin/activate"
+}
+
+# get rid of shims
+# if _command_exists pyenv; then
+#   eval "$(pyenv init - bash)"
+# fi
 
 # pyenv-virtualenv plugin is very slow
 # disable it in favor of ondir
