c183ec9dd53d3d858e0c7a0dadae8dd0710d947e
Anexen <avolk93@gmail.com>
2021-11-23 21:08:15 +0300

    fzf: bind to remove files

--- home/.config/bash/plugins/fzf.bash
+++ home/.config/bash/plugins/fzf.bash
@@ -12,7 +12,8 @@ else
 fi
 
 # fzf preview
-export FZF_DEFAULT_OPTS='--layout=reverse  --bind "alt-j:preview-down,alt-k:preview-up,ctrl-f:preview-page-down,ctrl-b:preview-page-up"'
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --no-ignore-vcs'
+export FZF_DEFAULT_OPTS='--layout=reverse --bind "alt-j:preview-down,alt-k:preview-up,ctrl-f:preview-page-down,ctrl-b:preview-page-up,alt-bs:execute-multi(rm {})+reload($FZF_DEFAULT_COMMAND)"'
 export FZF_CTRL_T_OPTS="--preview '(bat --color=always --style=plain {} || tree -C {}) 2> /dev/null | head -200'"
 
 # fzf rm
